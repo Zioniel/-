302af35a64529c04c8f59b84955062a7e7c1322b
Zioniel <rayangoosling@gmail.com>
2025-04-20 18:10:32 +0700

    таблица истинности (с пустотами)

---
+++ егэ /02 /01.py
@@ -0,0 +1,11 @@
+from itertools import *
+
+def f(x, y, z, w): return x and (z <= w) and (not y)
+
+for a in product([0, 1], repeat = 7):
+    table = [(a[0], a[1], 1, a[2]), (a[3], 1, 0, a[4]), (1, 0, a[5], a[6])] 
+    if len(table) == len(set(table)):
+        for p in permutations("xyzw"):
+            if [f(**dict(zip(p, r))) for r in table] == [1, 1, 1]: # хДхДхД лол лол лол лох
+                print(p)
+
