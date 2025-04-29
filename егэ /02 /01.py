bbf1b9da51457f861640e75a54c5ebc07cafc0d1
Zioniel <rayangoosling@gmail.com>
2025-04-29 13:49:25 +0700

    Update 01.py

--- егэ /02 /01.py
+++ егэ /02 /01.py
@@ -6,6 +6,6 @@ for a in product([0, 1], repeat = 7):
     table = [(a[0], a[1], 1, a[2]), (a[3], 1, 0, a[4]), (1, 0, a[5], a[6])] 
     if len(table) == len(set(table)):
         for p in permutations("xyzw"):
-            if [f(**dict(zip(p, r))) for r in table] == [1, 1, 1]: # хДхДхД лол лол лол лох
+            if [f(**dict(zip(p, r))) for r in table] == [1, 1, 1]: # хДхДхД лол лол лол 
                 print(p)
 
